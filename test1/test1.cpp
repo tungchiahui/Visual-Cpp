fee2d342b254efd0770c159750fd5d58f21e5c60
tungchiahui <tungchia-hui@foxmail.com>
2023-04-13 23:15:31 +0800

    tung

--- test1/test1.cpp
+++ test1/test1.cpp
@@ -1,15 +1,14 @@
+//test.cpp
 #include<stdio.h>
 #include<iostream>
 #include<stdlib.h>
 using namespace std;
 int main()
 {
-	float a;
-	cin>>a;
-	if(a<60)
-	cout<<"Fail"<<endl;
-	else
-	cout<<"Pass"<<endl;
+	float a,b;
+	cin>>a>>b;
+	cout<<a<<endl;
+	cout<<b<<endl;
 	system("pause");
 	return 0;
 }
